bc7e53775293f8bb6e3e52656c97b2d86e937c49
PolRenau14 <43928607+PolRenau14@users.noreply.github.com>
2019-10-08 11:25:30 +0200

    Add files via upload

--- Entrega1.R
+++ Entrega1.R
@@ -1,24 +1,4 @@
 
-#Some useful functions
-calcQ <- function(x) {
-  s.x <- summary(x)
-  iqr<-s.x[5]-s.x[2]
-  list(souti=s.x[2]-3*iqr, mouti=s.x[2]-1.5*iqr, min=s.x[1], q1=s.x[2], q2=s.x[3], 
-       q3=s.x[5], max=s.x[6], mouts=s.x[5]+1.5*iqr, souts=s.x[5]+3*iqr ) }
-
-countNA <- function(x) {
-  mis_x <- NULL
-  for (j in 1:ncol(x)) {mis_x[j] <- sum(is.na(x[,j])) }
-  mis_x <- as.data.frame(mis_x)
-  rownames(mis_x) <- names(x)
-  mis_i <- rep(0,nrow(x))
-  for (j in 1:ncol(x)) {mis_i <- mis_i + as.numeric(is.na(x[,j])) }
-  list(mis_col=mis_x,mis_ind=mis_i) }
-
-
-
-
-
 
 df<-read.table("adult.data",header=F, sep=",",fill=FALSE,              strip.white=TRUE,na.string="?")
 dim(df)
@@ -59,10 +39,39 @@ rm(list=ls())
 # Clear plots
 if(!is.null(dev.list())) dev.off()
 
+
+
+
+#Some useful functions
+calcQ <- function(x) {
+  s.x <- summary(x)
+  iqr<-s.x[5]-s.x[2]
+  list(souti=s.x[2]-3*iqr, mouti=s.x[2]-1.5*iqr, min=s.x[1], q1=s.x[2], q2=s.x[3], 
+       q3=s.x[5], max=s.x[6], mouts=s.x[5]+1.5*iqr, souts=s.x[5]+3*iqr ) }
+
+countNA <- function(x) {
+  mis_x <- NULL
+  for (j in 1:ncol(x)) {mis_x[j] <- sum(is.na(x[,j])) }
+  mis_x <- as.data.frame(mis_x)
+  rownames(mis_x) <- names(x)
+  mis_i <- rep(0,nrow(x))
+  for (j in 1:ncol(x)) {mis_i <- mis_i + as.numeric(is.na(x[,j])) }
+  list(mis_col=mis_x,mis_ind=mis_i) }
+
+
+
+
+
+
+
 # Command or Windows-like method
 load("mostra.RData")
 summary(df)
 
+
+#### Univariate Descriptive Analysis ##
+## Agruparem les variables descriptives en factors, no totes, les que creguem convienients
+##  Exploració de analisis de les dades.
 
 # Data Preparation
 
@@ -74,6 +83,13 @@ vars_dis<-names(df)[c(2,4,6:10,14:15)];vars_dis
 summary(df[,vars_con]) # Example of descriptive for numeric variables
 
 summary(df[,vars_dis])
+
+#veient el summary veiem per exemple que podem ajuntar les races, es a dir white or others.
+
+
+missingData<- which(is.na(df$race));length(missingData)
+
+
 
 
 levels(df$type.employer)
@@ -103,6 +119,122 @@ df$f.type<-factor(df$f.type,levels=1:4,labels=paste0("f.typ-",c("Civil","Private
 
 summary(df$f.type)
 
+barplot(table(df$f.type))
+
+
+
+
+##############
+
+
+
+levels(df$country)
+
+barplot(table(df$country))
+table(df$country)
+
+# decició conceptual:-> agrupem el que no es United-States.
+
+
+tapply(df$hr.per.week,df$country,mean)
+
+df$f.country<-1
+ll<-which(df$country == "United-States");length(ll)
+df$f.country[ll]<-2
+df[ll,"f.country"]<-2 # f.type already available
+
+# Define f.type as a factor and use 'nice' level names
+
+df$f.country<-factor(df$f.country,levels=1:2,labels=paste0("f.country-",c("No-United-States","United-States")))
+
+summary(df$f.country)
+
+barplot(table(df$f.country))
+
+##############
+
+#marital: 
+
+
+levels(df$marital)
+
+barplot(table(df$marital))
+table(df$marital)
+
+# decició conceptual:-> agrupem els cassats, no cassats(Divorciats, separats), 
+# altres ( viudes, i may cassats(assumint que normalment es gent jove els que mai son casats, y que viudus gent gran))
+
+
+tapply(df$hr.per.week,df$marital,mean)
+
+df$f.marital<-1
+ll<-which(df$marital %in% c("Married-AF-spouse","Married-civ-spouse","Married-spouse-absent"));length(ll)
+df$f.marital[ll]<-2
+df[ll,"f.marital"]<-2 # f.type already available
+ll<-which(df$marital %in% c ("Divorced","Separated")); length(ll)
+df$f.marital[ll]<-3
+df[ll,"f.marital"]<-3
+ll<-which(df$marital %in% c("Never-married","Widowed"))
+
+# Define f.type as a factor and use 'nice' level names
+
+df$f.marital<-factor(df$f.marital,levels=1:3,labels=paste0("f.marital-",c("Married","No- Married","Others")))
+
+summary(df$f.marital)
+
+barplot(table(df$f.marital))
+
+
+
+##############
+
+
+##############
+
+#occupation: 
+
+summary(df[,vars_dis])
+
+levels(df$occupation)
+
+barplot(table(df$occupation))
+table(df$occupation)
+
+# decició conceptual:-> agrupem el que no es United-States.
+
+
+tapply(df$hr.per.week,df$occupation,mean)
+
+df$f.country<-1
+ll<-which(df$country == "United-States");length(ll)
+df$f.country[ll]<-2
+df[ll,"f.country"]<-2 # f.type already available
+
+# Define f.type as a factor and use 'nice' level names
+
+df$f.country<-factor(df$f.country,levels=1:2,labels=paste0("f.country-",c("No-United-States","United-States")))
+
+summary(df$f.country)
+
+barplot(table(df$f.country))
+
+
+
+##############
+
+
+## Data Quality
+
+# per variable, contar  missing  num of errors num of outliers
+# rank variables =  missing+errors.
+
+
+## create variable  missing +  out + errors.
+
+
+
+
+
 
 
 levels(df$sex)<-paste0("Sex-",levels(df$sex))
@@ -113,10 +245,9 @@ summary(df$sex)
 prop.table(table(df$sex))
 
 # Graphic tools
-barplot(table(df$sex),main="Gender observations",col="orange")
 barplot(table(df$sex),main="Gender observations",col=rainbow(2))
-piechart(df$sex)
 
+## Variables numeriques detecció de outliers.
 
 summary(df$age)
 
@@ -148,8 +279,6 @@ var(df$age)
 sd(df$age)/mean(df$age)
 
 # Graphical tools
-hist(df$age)
-hist(df$age,main="Age histogram",col="orange")
 hist(df$age,main="Age histogram",col=rainbow(12))
 
 mm<-mean(df$age);dd<-sd(df$age);mm;dd
@@ -207,7 +336,8 @@ abline(h=calcQ(dff$hr.per.week)$souti,lwd=2,col="red",lty=2)
 abline(h=calcQ(dff$hr.per.week)$souts,lwd=2,col="red",lty=2)
 
 
-#library(missMDA)
+library(missMDA)
+
 summary(dff[,vars_con])  # Problem with capital_gain
 ll<-which(dff$capital.gain==99999)
 dff$capital.gain[ll]<-NA
@@ -218,4 +348,25 @@ par(mfrow=c(1,2))
 boxplot(dff$capital.gain)
 boxplot(res_num$completeObs[,"capital.gain"])
 par(mfrow=c(1,1))
+
+
+
+
+
+##############
+
+
+## Imputation
+
+# Eliminar els NA's.
+
+
+#############
+
+
+##  Profiling
+
+#numeric target
+
+#Factor(y)
 
