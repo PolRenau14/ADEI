9e1847d361ab476e56c28cfdc85971c020767eeb
PolRenau14 <pol.renau@est.fib.upc.edu>
2019-11-18 17:57:06 +0100

    Entrega2

--- Entrega2/Projecte2/Entrega2.R
+++ Entrega2/Projecte2/Entrega2.R
@@ -3,14 +3,94 @@ load("mostra2.RData")
 summary(df)
 library(FactoMineR)
 library(car)
-#CA
 
 #hourperweek discretitzation (fact) vs education
 
 names(df)
+vars_con<-names(df)[c(3,5,11:13)];vars_con
+vars_dis<-names(df)[c(7,9,10,15:23)];vars_dis
 
 
+#PCA Analysis -----------------------------------------------------------------
 
+#Fem l'analisi incialment sobre totes les variables
+res.pca <- PCA(df[,c(1,3,5,11:13)])
+
+#Display dels Eigenvalues i % explicat amb cada dimensio, veiem que segons
+#kaiser hauriem d'agafar les primeres tres dimensions, que expliquen un 57.82% de la inertia 
+summary(res.pca,nb.dec=2,nbelements = Inf,nbind = 0)
+
+#Plot per veure quines dim son >1 per explicar-les pel criteri de kaiser
+barplot(res.pca$eig[,1])
+abline(h= 1,col="red",lty=2)
+#Veiem que necesitem les 3 primeres dimensions segons el criteri de Kaiser.
+
+#Ara cercem els individus que mes contribueixen. Seran els que queden mes als extrems del primer plot
+#Al segon plot busquem els 10 que mes contribueixen i el seu valor
+
+#choix="ind" for the individual variables
+#select = "contrib 10" per seleccionar els 10 individus que tenen una major contribucio en el plot en dos dimensions
+plot(res.pca,choix="ind",label="none",col.ind="grey80")
+#cex=0.5 per escalar els elements d'aquest plot per a que es vegin mes petits
+plot(res.pca,choix="ind",select="contrib 10",cex=0.5)
+
+
+#Interpretacio dels axis: 3 primers? (per kaiser)
+
+#Dim 1
+Boxplot(res.pca$ind$contrib[,1])
+rang1<-order(res.pca$ind$contrib[,1],decreasing = T); rang1[1:10]
+rownames(df[rang1[1:10],])
+ll<-which(res.pca$ind$coord[,2]<(-5));length(ll)
+df[ll,c(vars_con,vars_dis)]
+
+#Dim 2
+Boxplot(res.pca$ind$contrib[,2])
+rang1<-order(res.pca$ind$contrib[,2],decreasing = T); rang1[1:10]
+rownames(df[rang1[1:10],])
+ll<-which(res.pca$ind$coord[,2]<(-5));length(ll)
+df[ll,c(vars_con,vars_dis)]
+
+#Dim 3
+Boxplot(res.pca$ind$contrib[,3])
+rang1<-order(res.pca$ind$contrib[,3],decreasing = T); rang1[1:10]
+rownames(df[rang1[1:10],])
+ll<-which(res.pca$ind$coord[,2]<(-5));length(ll)
+df[ll,c(vars_con,vars_dis)]
+
+#Dubto en quines variables agafar com a quanti i quali suplementaries
+names(df)
+res.pca <- PCA(df[,c(1,3,5,11:13,15:24)], quanti.sup=6,quali.sup =  7:16)
+res.pca$eig
+
+#resolució gràfica. Hem d'agafar les 3 primeres components.
+colors<- c("Blue", "Orange")
+barplot(res.pca$eig[,1], col = colors[ifelse(res.pca$eig[,1] > 1, 1,2)])
+legend("bottomright", legend=c("Superior o igual 1", "Inferior 1"), title="Eig Val", col = colors,
+       pch=19)
+abline(h= 1,col="red",lty=2)
+
+# Per criteri de Elbow's sabem que hem d'agafar 4 Clausters. ( aquelles components que expliquen el 80 % de la variancia)
+
+barplot(res.pca$eig[,3],col= colors[ifelse(res.pca$eig[,3] < 85, 1, 2)])
+abline(h=80,col="red",lty=2)
+
+
+#K-Means Classification-------------------------------------------------
+#Kmeans només es pot fer per aquelles variables que són numeriques.
+install.packages("factoextra")
+library(factoextra)
+num.clusters<- 5
+names(df)
+df$f.hpw<- factor(df$f.hpw)
+df$f.educationNum <- factor(df$f.educationNum)
+df.numeriques <- df[,c(1,3,5,11:13)]
+kmeans.res <- kmeans(df.numeriques,4)
+summary(kmeans.res$centers)
+
+#Hierarchical Clustering------------------------------------------------
+
+res.hcpc<-HCPC(res.pca,nb.clust=5,graph=T) 
 
 
 
@@ -52,9 +132,14 @@ aux<-res.mca1$quali.sup$coord[2:5,1]
 aux<-aux +res.mca1$quali.sup$coord[1,1]
 lines(aux,col="darkgreen",lwd=2)
 res.mca1$quali.sup$coord
+fviz_mca_var(res.mca1, choice = "mca.cor", 
+             repel = TRUE, # Avoid text overlapping (slow)
+             ggtheme = theme_minimal())
 
 
 mean(res.mca1$eig[,1])
 
 res.mca2<-MCA(df[,c(13,22,15:21,10)],quanti.sup=1,quali.sup = 2:3,ncp=14)
 res.hc<-HCPC(res.mca2,nb.clust = 0)
+
+
